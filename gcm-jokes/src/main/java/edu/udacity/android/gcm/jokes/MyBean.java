c735c6d527bdb04420b7968b6959813577b02a08
Shamim Ahmed <shamim.ahmed@sandstone.com.au>
2016-01-17 21:48:25 +1100

    added a new google cloud module

---
+++ gcm-jokes/src/main/java/edu/udacity/android/gcm/jokes/MyBean.java
@@ -0,0 +1,15 @@
+package edu.udacity.android.gcm.jokes;
+
+/** The object model for the data we are sending through endpoints */
+public class MyBean {
+
+    private String myData;
+
+    public String getData() {
+        return myData;
+    }
+
+    public void setData(String data) {
+        myData = data;
+    }
+}
