8f19c66dd056b59f567ed7f494e3c99d6956e668
Shamim Ahmed <shamim.buet.99@gmail.com>
2016-01-24 00:03:36 +1100

    added some logging to the custom context listener

--- gcm-jokes/src/main/java/edu/udacity/android/gcm/jokes/listener/CustomContextListener.java
+++ gcm-jokes/src/main/java/edu/udacity/android/gcm/jokes/listener/CustomContextListener.java
@@ -1,19 +1,32 @@
 package edu.udacity.android.gcm.jokes.listener;
 
+import java.util.logging.Logger;
+
 import javax.servlet.ServletContextEvent;
 import javax.servlet.ServletContextListener;
 
+import edu.udacity.model.Joke;
 import edu.udacity.provider.jokes.JokeProvider;
 
 public class CustomContextListener implements ServletContextListener {
+    private final Logger logger = Logger.getLogger(CustomContextListener.class.getCanonicalName());
+
     @Override
     public void contextInitialized(ServletContextEvent sce) {
+        logger.info("The context is being initialized...");
         // Initiate a joke retrieval so that all jokes are loaded
         // This will ensure that the first client does not experience significant delay
-        JokeProvider.getJoke();
+        Joke joke = JokeProvider.getJoke();
+
+        if (joke != null) {
+            logger.info(String.format("The internally retrieved joke is : %s", joke.getContent()));
+        } else {
+            logger.warning("No joke was retrieved");
+        }
     }
 
     @Override
     public void contextDestroyed(ServletContextEvent sce) {
+        logger.info("The context is being destroyed...");
     }
 }
