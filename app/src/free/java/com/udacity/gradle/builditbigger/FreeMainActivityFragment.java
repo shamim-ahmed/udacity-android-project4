e2cb02df4208e295494d9db4da9ee4a1c62977e5
Shamim Ahmed <shamim.buet.cse.99@gmail.com>
2016-01-31 16:57:41 +1100

    now an interstitial ad is shown when the button is clicked

--- app/src/free/java/com/udacity/gradle/builditbigger/FreeMainActivityFragment.java
+++ app/src/free/java/com/udacity/gradle/builditbigger/FreeMainActivityFragment.java
@@ -6,21 +6,39 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 
+import com.google.android.gms.ads.AdListener;
 import com.google.android.gms.ads.AdRequest;
 import com.google.android.gms.ads.AdView;
+import com.google.android.gms.ads.InterstitialAd;
 
 /**
  * A placeholder fragment containing a simple view.
  */
 public class FreeMainActivityFragment extends AbstractFragment {
+    private InterstitialAd interstitialAd;
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         View root = inflater.inflate(R.layout.fragment_main_free, container, false);
 
+        // register click listener for the joke button
         Button jokeButton = (Button) root.findViewById(R.id.joke_button);
         jokeButton.setOnClickListener(this);
 
+        // configure interstitial ad
+        interstitialAd = new InterstitialAd(getActivity());
+        interstitialAd.setAdUnitId("ca-app-pub-3940256099942544/1033173712");
+        interstitialAd.setAdListener(new AdListener() {
+            @Override
+            public void onAdClosed() {
+                requestNewInterstitial();
+            }
+        });
+
+        requestNewInterstitial();
+
+        // configure ad shown at the bottom of the fragment
         AdView mAdView = (AdView) root.findViewById(R.id.adView);
         // Create an ad request. Check logcat output for the hashed device ID to
         // get test ads on a physical device. e.g.
@@ -30,5 +48,16 @@ public class FreeMainActivityFragment extends AbstractFragment {
                 .build();
         mAdView.loadAd(adRequest);
         return root;
+    }
+
+    @Override
+    public void onClick(View v) {
+        super.onClick(v);
+        interstitialAd.show();
+    }
+
+    private void requestNewInterstitial() {
+        AdRequest request = new AdRequest.Builder().addTestDevice("D55E75550E2CA393D1269880F1D4D4FC").build();
+        interstitialAd.loadAd(request);
     }
 }
