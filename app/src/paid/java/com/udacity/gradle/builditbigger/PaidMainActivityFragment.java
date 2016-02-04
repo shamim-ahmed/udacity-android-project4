6f728bed7aeca4f25f49b89e879580c22499be45
Shamim Ahmed <shamim.buet.cse.99@gmail.com>
2016-02-04 18:46:10 +1100

    added the circular progress bar in the paid version of the app

--- app/src/paid/java/com/udacity/gradle/builditbigger/PaidMainActivityFragment.java
+++ app/src/paid/java/com/udacity/gradle/builditbigger/PaidMainActivityFragment.java
@@ -5,11 +5,13 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.ProgressBar;
 
 /**
  * A placeholder fragment containing a simple view.
  */
 public class PaidMainActivityFragment extends AbstractFragment {
+    private ProgressBar progressBar;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -19,6 +21,20 @@ public class PaidMainActivityFragment extends AbstractFragment {
         Button jokeButton = (Button) root.findViewById(R.id.joke_button);
         jokeButton.setOnClickListener(this);
 
+        progressBar = (ProgressBar) root.findViewById(R.id.progress_bar);
+
         return root;
+    }
+
+    @Override
+    public void onClick(View v) {
+        super.onClick(v);
+        progressBar.setVisibility(View.VISIBLE);
+    }
+
+    @Override
+    public void onSuccess(String data) {
+        super.onSuccess(data);
+        progressBar.setVisibility(View.GONE);
     }
 }
