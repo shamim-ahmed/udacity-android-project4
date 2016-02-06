5fa9820d1515ab787b212a55834f365f4d0b14b6
Shamim Ahmed <shamim.buet.99@gmail.com>
2016-02-06 21:39:03 +1100

    updated the maximum duration for which the button may stay disabled

--- app/src/main/java/edu/udacity/gradle/builditbigger/util/Constants.java
+++ app/src/main/java/edu/udacity/gradle/builditbigger/util/Constants.java
@@ -1,7 +1,7 @@
 package edu.udacity.gradle.builditbigger.util;
 
 public class Constants {
-    public static final int JOKE_FETCH_MAX_DELAY = 10000;
+    public static final int JOKE_FETCH_MAX_DELAY = 20000;
 
     // private constructor to prevent instantiation
     private Constants() {
